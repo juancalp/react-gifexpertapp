257d010c80bd34e5be45e38a5bd7090a8237c1d1
Juan Carlos Lopez <juancalp@gmail.com>
2020-10-08 23:11:16 -0500

    proyecto con docs

---
+++ docs/precache-manifest.c3ffb5b8fcc339407ea08c30feed78d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdf72db0a31bb33ae55471ec41098a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09305a4e8b9bf59cf88d",
+    "url": "/static/css/main.6170e940.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09305a4e8b9bf59cf88d",
+    "url": "/static/js/main.0cfd57e6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
